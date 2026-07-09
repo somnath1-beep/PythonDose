2bee6356e684a2588f6d6eda648b0cd469d19a79
Somnath Yadav <ysomnath166@gmail.com>
2026-07-09 15:17:23 +0545

    File MOdified and to keep record not in normal string rather in base structure and some bugs is fixed

---
+++ RecordBook.py
@@ -0,0 +1,277 @@
+import shelve as sh, re,os
+import tkinter as tk
+from tkinter import messagebox,simpledialog
+import base64 # to convert binary data to scramble object 
+#match pattern for password validation
+#to decode Base64 structure into bytes then string
+def text_manipulate(scramble_obj):
+    #Decode the Base64 structure back to standard bytes
+    decoded_key_bytes = base64.b64decode(scramble_obj)
+    # decode bytes to original string
+    final_key = decoded_key_bytes.decode('utf-8')
+    return final_key
+def file_operation(file,u_keys=None,u_values=None,mode=None):#if you have to write pass keys and values list and file path
+    # else for read pass file path and mode
+    if mode=='r'or mode=='r+':
+        if os.path.exists(file):
+            fileobj=open(file,f'{mode}')
+            keys=[]
+            values=[]
+            for file_line in fileobj.readlines():
+                line_data=file_line.strip().split(':')
+                scramble_key=line_data[0].strip('"').lstrip('b')# scramble obj contain "b'X2luZm9fcGFzc3dvcmQ='" like format
+                # that's why i stripped " from both side and then b other "b'X2luZm9fcGFzc3dvcmQ='" can't be decoded by utf-8 sceme
+                scramble_value=line_data[1].strip('"').lstrip('b')
+                final_key=text_manipulate(scramble_key)
+                final_value=text_manipulate(scramble_value)
+                keys.append(final_key)
+                values.append(final_value)
+            fileobj.close()           
+            return keys, values#return list of keys and values from file
+        else:
+            return 'Invalid Path!'
+    elif mode=='a' or mode==None or mode=='w+':# binary mode 
+        fileobj=open(file,f'{mode}')
+        for i in range(0,len(u_keys)):
+            binary_u_keys=u_keys[i].encode("utf-8")#encoding in binary
+            binary_u_values=u_values[i].encode("utf-8")
+            scramble_binary_u_keys=base64.b64encode(binary_u_keys)#encoding in scramble obj
+            scramble_binary_u_values=base64.b64encode(binary_u_values)
+            fileobj.write(f'{scramble_binary_u_keys}:{scramble_binary_u_values}\n')
+        fileobj.close()
+        return 'success'#for writting completition
+
+
+def filehandler(name,search=None,status='create'):#make folder and search path return file path and true /false
+    if search==None and status=='create':# for new file, make folder and return path of file
+        folder=os.path.join('c:\\','RecordBook',f'info_{name}')
+        file=os.path.join(folder,f'{name}.txt')# .dat -> generic data
+        if os.path.exists(folder):
+            return file
+        else:
+            os.makedirs(folder)
+            return file
+    elif search==None and status!='create': # for update or view . return file path
+        file=os.path.join('c:\\','RecordBook',f'info_{name}',f'{name}.txt')
+        return file
+    elif search=='searching':# for search any file associated with given name and return true /false if found or not
+        file=os.path.join('c:\\','RecordBook',f'info_{name}',f'{name}.txt')
+        #folder=os.path.join('c:\\','Datarecorder',f'info_{name}')
+        search_result=os.path.exists(file)  
+        return search_result
+    
+
+password_pattern=re.compile(r'''\w+\d+[!@#$%^&*()_+{}":;\']+\w+\d+|\d+\w+[!@#$%^&*()_+{}":;\']+\w+\d+
+                            \w+[!@#$%^&*()_+{}":;\']+\w+\d+|\w+\d+[!@#$%^&*()_+{}":;\']+\d+|
+                            \w+\d+[!@#$%^&*()_+{}":;\']+\w+|[!@#$%^&*()_+{}":;\']+\w+\d+|[!@#$%^&*()_+{}":;\']+\d+\w+|
+                            \d+[!@#$%^&*()_+{}":;\']+\w+\d+|[!@#$%^&*()_+{}":;\']+\w+\d+|\d+[!@#$%^&*()_+{}":;\']+\w+\d+|
+                            \\d+\w+[!@#$%^&*()_+{}":;\']+\d+\w+''')# to valdiate password that contain at least one alphabet, one digit, and one special character.
+
+#validate password function
+def validate_password(password):
+        if(len(password)<8):# for insufficient length
+                verification_message="Password must be at least 8 character"
+                return verification_message,0
+                
+        password_match=password_pattern.findall(password)
+        if password_match!=[]:# if password validate successfully
+                return  'Password Verified',1
+        else:
+            verification_message="Password must contain at least one alphabet, one digit, and one special character."
+            return verification_message,0
+        
+        
+#check if file exists and update or view information
+def update_info(name,password):# to add or delete data from /in file
+    filepath=filehandler(name,status='update')#return file path
+    ret=file_operation(filepath,mode='r')#return keys and values from file in tuple
+    if ret=='Invalid Path!':
+        messagebox.showerror("File Search",f"{ret}")
+        return
+    filekeys=ret[0]# accessing list from tuple
+    filevalues=ret[1] # accessing list from tuple 
+    if filevalues[0]==password:#matching password
+        root.lift()# bring main tkinter window automatically on top of desktop
+        root.focus_force()# bring any popup window wiating for input from keyboard in focus
+        modification_choice=simpledialog.askstring(" File Modification",'''
+                                                    1.Delete Data     
+                                                    2.Add Data          ''',parent=root)
+        if modification_choice==1:
+            root.lift()# bring main tkinter window automatically on top of desktop
+            root.focus_force()# bring any popup window wiating for input from keyboard in focus
+            delete_key=information=simpledialog.askstring("Delete keys","Enter Keys to be deleted:      ",parent=root).split(",")#get list of keys to be deleted by user
+            InvalidKeys=[]# for keys that not found in file given by user to delete
+            for i in delete_key:
+                if i in filekeys:
+                    key_frequency=filekeys.count(i)
+                    for j in range(key_frequency):
+                        index=filekeys.index(i)
+                        del filekeys[index]
+                        del filevalues[index]
+                else:
+                    InvalidKeys.append(i)#appending  key not found
+            file_operation(filepath,u_keys=filekeys,u_values=filevalues,mode='w+')
+            messagebox.showinfo("Delete Keys","Deletion successfull!")
+            if InvalidKeys!=[]:# show key that not found 
+                messagebox.showwarning("Delete Keys",f"Keys Not Found:{InvalidKeys}")
+            return
+        else:#adding data
+            root.lift()# bring main tkinter window automatically on top of desktop
+            root.focus_force()# bring any popup window wiating for input from keyboard in focus
+            userkeys=simpledialog.askstring("Enter category","e.g., name,address,age:      ",parent=root).split(",")
+            uservalues=[]
+            for i in range(0,len(userkeys)):
+                root.lift()# bring main tkinter window automatically on top of desktop
+                root.focus_force()# bring any popup window wiating for input from keyboard in focus
+                info=simpledialog.askstring("Enter Your Details",f"Enter your {userkeys[i]}:       ",parent=root)
+                uservalues.append(info)# values appended give by user
+            file_operation(filepath,u_keys=userkeys,u_values=uservalues,mode='a')
+            messagebox.showinfo("File Modification","File Modified!")
+            return
+    else:
+        messagebox.showerror("Password Verification","Wrong Password!")
+        return        
+    
+def view_info(name,password):# display your saved information in the file
+    filepath=filehandler(name,status='view')
+    ret=file_operation(filepath,mode='r')#return keys and values or Invalid Path!
+    if ret=='Invalid Path!':
+        messagebox.showerror("File Search",f"{ret}")
+        return
+    filekeys=ret[0]
+    filevalues=ret[1]    
+    if filevalues[0]==password:
+        widget=root.winfo_children()
+        if len(widget)==8:
+            output.delete("1.0",tk.END)
+        else:
+            output_widget()
+        for i in range(1,len(filekeys)):
+            output.insert(tk.END,f"{filekeys[i]}:{filevalues[i]}\n")# displaying output in window
+    
+    else:
+        messagebox.showerror("Password Verification","Wrong Password!")
+        return
+    return
+
+def new_info(name,password):# create a new file and save your information in it
+    filepath=filehandler(name)
+    file_operation(filepath,u_keys=['_info_password'],u_values=[password],mode='w+')
+    root.lift()# bring main tkinter window automatically on top of desktop
+    root.focus_force()# bring any popup window wiating for input from keyboard in focus
+    userkeys=simpledialog.askstring("Enter category","e.g., name,address,age:      ",parent=root).split(",")
+    uservalues=[]
+    for i in range(0,len(userkeys)):
+        root.lift()# bring main tkinter window automatically on top of desktop
+        root.focus_force()# bring any popup window wiating for input from keyboard in focus
+        info=simpledialog.askstring("Enter Your Details",f"Enter your {userkeys[i]}:       ",parent=root)
+        uservalues.append(info)# values appended give by user
+    file_operation(filepath,u_keys=userkeys,u_values=uservalues,mode='a')
+    messagebox.showinfo("Create","File Created Successfully!")
+    return
+
+def backend(Name=None, password=None):
+    name=Name.lower()
+    
+    password_result=validate_password(password)#rreturn tupple with message and 1/0 1-> success
+    if password_result[1]==0:# 0 means password doesn't meet criteria
+         messagebox.showinfo("Password Verification",password_result[0])
+         return
+    root.lift()# bring main tkinter window automatically on top of desktop
+    root.focus_force()# bring any popup window wiating for input from keyboard in focus
+    option=simpledialog.askinteger("Select OPtion",''' 
+1.for new file
+2.for update file 
+3.for view info      ''',parent=root)
+    if option==1:
+         if(filehandler(name,'searching')):
+              root.lift()# bring main tkinter window automatically on top of desktop
+              root.focus_force()# bring any popup window wiating for input from keyboard in focus
+              replace=simpledialog.askinteger("File Searching","""File Already Exist!
+                                            Do you want to Replace file
+                                            Enter 1 for yes 0 for no      """,parent=root)
+              if replace==1:
+                   file=os.path.join('c:\\','Datarecorder',f'info_{name}',f'{name}.txt')
+                   os.remove(file) 
+                   new_info(name,password)#replace existed file
+                   return
+              else:
+                   return  
+         new_info(name,password)# create a new file if file not existed already
+         
+    elif option==2:
+         if filehandler(name,'searching'):
+            update_info(name,password)
+         else: 
+            messagebox.showerror("File Searching","File not found!")
+            return
+    elif  option==3 :
+         if filehandler(name,'searching'):
+             view_info(name,password)
+         else: 
+            messagebox.showerror("File Searching","File not found!")
+            return
+        
+    
+def name_widget():
+    global name_entry
+    namelbl=tk.Label(root,text="Enter your Name:")
+    namelbl.pack(pady=10)
+    name_entry=tk.Entry(root,width=40,bg="cyan",fg="black")
+    name_entry.pack(pady=10)
+
+def password_widget():
+    global  password_entry
+    passwordlbl=tk.Label(root,text="Password:")
+    passwordlbl.pack(pady=10)
+    password_entry=tk.Entry(root,width=40, show="*", bg='cyan')
+    password_entry.pack(pady=10)
+
+def clean_widget():
+    for widget in root.winfo_children():
+        widget.destroy()
+
+
+def send_btn(function, head):
+    global submit_button
+    submit_btn=tk.Button(root,text=head, command=function)
+    submit_btn.pack(pady=10)
+
+def getinfo():
+    global name, password
+    name=name_entry.get()
+    password=password_entry.get()
+    #if user not inputed any onr field then no operation will perfomed
+    if name==''or password=='':
+        return
+    backend(name,password)
+
+def password_validate_widget():
+    clean_widget()
+    name_widget()
+    password_widget()
+
+def output_widget():
+    global output
+    outputlbl=tk.Label(root,text="Output:")
+    outputlbl.pack(pady=10)
+    output=tk.Text(root, width=50, height=100,bg='cyan',fg='black')
+    output.pack(pady=10)
+    return
+
+def main_interface():
+    clean_widget()
+    name_widget()
+    password_widget()
+    send_btn(getinfo,"ok")
+    send_btn(main_interface,"clear screeen")
+root=tk.Tk()
+root.title("Information Recorder")
+root.geometry("500x500")
+main_interface()
+
+root.mainloop()
+
+    
+
+#create / make name and password global 
